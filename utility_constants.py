453fed19353082ec55c583b68a08c295274350c8
Geoff Munn <geoff@wxoutside.tools>
2023-03-10 21:41:44 +1300

    gas adjustment back to 3.5

--- utility_constants.py
+++ utility_constants.py
@@ -7,8 +7,7 @@ WITHDRAWAL_REMAINDER = 100   # This is the amount of Lunc we want to keep after 
 GAS_PRICE_URI       = 'https://fcd.terra.dev/v1/txs/gas_prices'
 TAX_RATE_URI        = 'https://lcd.terra.dev/terra/treasury/v1beta1/tax_rate'
 CONFIG_FILE_NAME    = 'user_config.yml'
-#GAS_ADJUSTMENT      = 3.5
-GAS_ADJUSTMENT      = 1
+GAS_ADJUSTMENT      = 3.5
 
 # Swap contracts can be found here
 # https://assets.terra.money/cw20/pairs.dex.json
