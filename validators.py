54ad1f0e25bb36fdc7865ca1ceab6d7f8c68fc63
Geoff Munn <geoff@wxoutside.tools>
2023-03-17 14:14:37 +1300

    Delegation option nearly working

--- validators.py
+++ validators.py
@@ -50,7 +50,7 @@ def get_user_choice(question:str, yes_choices:list, no_choices:list) -> str|bool
 
     return result
 
-def get_user_number(question:str) -> int:
+def get_user_number(question:str, max_number:int) -> int:
     """
     Get ther user input - must be a number.
     """ 
@@ -58,9 +58,11 @@ def get_user_number(question:str) -> int:
     while True:    
         answer = input(question).strip(' ')
         if answer.isdigit():
-            break
 
-    return answer
+            if int(answer) > 0 and int(answer) <= max_number:
+                break
+
+    return int(answer)
 
 def get_user_multichoice(question:str, user_wallets:dict) -> dict|str:
     """
@@ -116,23 +118,75 @@ def get_user_singlechoice(question:str, user_wallets:dict) -> dict|str:
     This is a custom function because the options are specific to this list.
     """
 
+    label_widths = []
+
+    label_widths.append(len('Number'))
+    label_widths.append(len('Wallet name'))
+    label_widths.append(len('LUNC'))
+    label_widths.append(len('USTC'))
+
+    for wallet_name in user_wallets:
+        if len(wallet_name) > label_widths[1]:
+            label_widths[1] = len(wallet_name)
+
+        if len(str(user_wallets[wallet_name].balances['uluna'])) > label_widths[2]:
+            label_widths[2] = len(str(user_wallets[wallet_name].balances['uluna']))
+
+        if len(str(user_wallets[wallet_name].balances['uusd'])) > label_widths[3]:
+            label_widths[3] = len(str(user_wallets[wallet_name].balances['uusd']))
+
+    padding_str = ' ' * 100
+
+    header_string = ' Number |'
+
+    if label_widths[1] > len('Wallet name'):
+        header_string +=  ' Wallet name' + padding_str[0:label_widths[1]-len('Wallet name')] + ' '
+    else:
+        header_string +=  ' Wallet name '
+
+    if label_widths[2] > len('LUNC'):
+        header_string += '| LUNC ' + padding_str[0:label_widths[2]-len('LUNC')] + ' '
+    else:
+        header_string += '| LUNC '
+
+    if label_widths[3] > len('USTC'):
+        header_string += '| USTC'  + padding_str[0:label_widths[3]-len('USTC')] + ' '
+    else:
+        header_string += '| USTC '
+
+    horizontal_spacer = '-' * len(header_string)
+
     wallets_to_use = {}
     while True:
 
         count = 0
         wallet_numbers = {}
 
+        print (horizontal_spacer)
+        print (header_string)
+        print (horizontal_spacer)
+
         for wallet_name in user_wallets:
+            wallet:Wallet  = user_wallets[wallet_name]
+
             count += 1
-            wallet_numbers[count] = user_wallets[wallet_name]
+            wallet_numbers[count] = wallet
                 
             if wallet_name in wallets_to_use:
                 glyph = '✅'
             else:
-                glyph = ''
+                glyph = '  '
 
-            print (f"  ({count}) {glyph} {wallet_name}")
+            count_str =  f' {count}' + padding_str[0:6 - (len(str(count)) + 2)]
             
+            wallet_name_str = wallet_name + padding_str[0:label_widths[1] - len(wallet_name)]             
+            lunc_str = wallet.formatUluna(wallet.balances['uluna'], False)
+            ustc_str = wallet.formatUluna(wallet.balances['uusd'], False)
+            
+            print (f"{count_str}{glyph} | {wallet_name_str} | {lunc_str} | {ustc_str}")
+            
+        print (horizontal_spacer + '\n')
+
         answer = input(question).lower()
         
         if answer.isdigit() and int(answer) in wallet_numbers:
@@ -154,7 +208,105 @@ def get_user_singlechoice(question:str, user_wallets:dict) -> dict|str:
         if answer == 'q':
             break
 
-    return wallets_to_use, answer
+    # Get the first (and only) validator from the list
+    for item in wallets_to_use:
+        user_wallet = wallets_to_use[item]
+        break;
+    
+    return user_wallet, answer
+
+def get_validator_singlechoice(question:str, validators:dict) -> dict|str:
+    """
+    Get a single user selection from a list.
+    This is a custom function because the options are specific to this list.
+    """
+
+    # Get the longest validator name:
+    label_widths = []
+
+    label_widths.append(len('Number'))
+    label_widths.append(len('Validator name'))
+    label_widths.append(len('Commission'))
+    label_widths.append(len('Voting power'))
+
+    for validator_name in validators:
+        if len(validator_name) > label_widths[1]:
+            label_widths[1] = len(validator_name)
+
+    padding_str = ' ' * 100
+
+    header_string = ' Number |'
+
+    if label_widths[1] > len('Validator name'):
+        header_string +=  ' Commission | Voting power | Validator name' + padding_str[0:label_widths[1]-len('Validator name')]
+    else:
+        header_string +=  ' Commission | Voting power | Validator name'
+
+    horizontal_spacer = '-' * len(header_string)
+
+    validators_to_use = {}
+
+    while True:
+
+        count = 0
+        validator_numbers = {}
+
+        print (horizontal_spacer)
+        print (header_string)
+        print (horizontal_spacer)
+        
+        for validator_name in validators:
+            count += 1
+            validator_numbers[count] = validators[validator_name]
+                
+            if validator_name in validators_to_use:
+                glyph = '✅'
+            else:
+                glyph = '  '
+
+            voting_power = str(round(validators[validator_name]['voting_power'],2)) + '%'
+            commission = str(validators[validator_name]['commission']) + '%'
+
+            count_str =  f' {count}' + padding_str[0:6 - (len(str(count)) + 2)]
+            
+            validator_name_str = ' ' + validator_name + padding_str[0:label_widths[1] - len(validator_name)]             
+            commission_str = commission + padding_str[0:label_widths[2] - len(commission)]
+            voting_power_str = ' ' + voting_power + padding_str[0:label_widths[3] - len(voting_power)]
+
+            print (f"{count_str}{glyph} | {commission_str} |{voting_power_str} |{validator_name_str}")
+
+            if count == utility_constants.MAX_VALIDATOR_COUNT:
+                break
+            
+        print (horizontal_spacer + '\n')
+
+        answer = input(question).lower()
+        
+        if answer.isdigit() and int(answer) in validator_numbers:
+
+            validators_to_use = {}
+
+            key = validator_numbers[int(answer)]['moniker']
+            if key not in validators_to_use:
+                validators_to_use[key] = validator_numbers[int(answer)]
+            else:
+                validators_to_use.pop(key)
+            
+        if answer == 'x':
+            if len(validators_to_use) > 0:
+                break
+            else:
+                print ('\nPlease select a validator first.\n')
+
+        if answer == 'q':
+            break
+
+    # Get the first (and only) validator from the list
+    for item in validators_to_use:
+        user_validator = validators_to_use[item]
+        break;
+
+    return user_validator, answer
 
 def coin_list(input: Coins, existingList: dict) -> dict:
     """ 
@@ -170,12 +322,129 @@ def coin_list(input: Coins, existingList: dict) -> dict:
 
 def main():
 
-    validators = Validators().create(True)
-    
-    for validator in validators:
-        if validators[validator]['is_jailed'] == False:
-            print(validator, validators[validator]['is_jailed'])
 
+    #DONE: step 1: select one wallet (show balances against wallet)
+    #step 2: select (delegate, undelegate, switch)
+    #step 2.1: delegate:
+    #DONE: step 2.1.1: select one validator
+    #step 2.1.2: select amount to delegate
+    #step 2.2: undelegate:
+    #step 2.2.1 select one validator
+    #step 2.2.2 confirm that undelegation is required
+    #step 2.3: switch
+    #step 2.3.1: select current validator
+    #step 2.3.2: select new validator
+    #step 2.3.3: confirm that switch is required
+
+
+    # Get the password that decrypts the user wallets
+    decrypt_password:str = getpass() # the secret password that encrypts the seed phrase
+
+    try:
+        with open(utility_constants.CONFIG_FILE_NAME, 'r') as file:
+            user_config = yaml.safe_load(file)
+    except :
+        print (' 🛑 The user_config.yml file could not be opened - please run configure_user_wallets.py before running this script')
+        exit()
+
+    print ('Decrypting and validating wallets - please wait...\n')
+
+    # Create the wallet object based on the user config file
+    wallet_obj = Wallets().create(user_config, decrypt_password)
+    
+    # Get all the wallets
+    user_wallets = wallet_obj.getWallets(True)
+
+    # Get the balances on each wallet (for display purposes)
+    for wallet_name in user_wallets:
+        wallet:Wallet = user_wallets[wallet_name]
+        wallet.getBalances()
+
+    if len(user_wallets) > 0:
+        print (f'You can send LUNC on the following wallets:')
+
+        wallet,answer = get_user_singlechoice("Select a wallet number 1 - " + str(len(user_wallets)) + ", 'X' to continue', or 'Q' to quit: ", user_wallets)
+
+        if answer == 'q':
+            print (' 🛑 Exiting...')
+            exit()
+    else:
+        print (' 🛑 This password couldn\'t decrypt any wallets. Make sure it is correct, or rebuild the wallet list by running the configure_user_wallet.py script again.')
+        exit()
+
+    # Get the desired actions
+    print ('\nWhat action do you want to take?')
+    print ('  (D)  Delegate to a validator')
+    print ('  (U)  Undelegate all coins from a validator')
+    print ('  (S)  Switch validators')
+    print ('  (Q)  Quit')
+    
+    user_action = get_user_choice('', ['d', 'u', 's', 'q'], [])
+
+    if user_action == 'q':
+        print (' 🛑 Exiting...')
+        exit()
+
+    print ('\nGetting available validators - please wait...')
+
+    validators = Validators()
+    validators.create()
+
+    sorted_validators = validators.sorted_validators
+
+    if len(sorted_validators) == 0:
+        print (' 🛑 No validators could be retrieved - perhaps there are network issues?')
+        exit()
+
+    if user_action == utility_constants.USER_ACTION_VALIDATOR_DELEGATE:
+
+        print (f'Select a validator to delegate to:')
+
+        user_validator, answer = get_validator_singlechoice("Select a validator number 1 - " + str(len(sorted_validators)) + ", 'X' to continue', or 'Q' to quit: ", sorted_validators)
+
+        if answer == 'q':
+            print (' 🛑 Exiting...')
+            exit()
+
+        print (f"The {wallet.name} wallet holds {wallet.formatUluna(wallet.balances['uluna'], True)}")
+        delegated_lunc:int = get_user_number('How much are you delegating? ', int(wallet.balances['uluna']))
+        delegated_uluna:int = delegated_lunc * utility_constants.COIN_DIVISOR
+
+        print (f'Delegating {delegated_lunc}...')
+        
+        delegation_tx = wallet.delegate().create(wallet.address, user_validator['operator_address'])
+
+        # Simulate it
+        result = delegation_tx.simulate(delegated_uluna)
+
+        if result == True:
+                
+            print (delegation_tx.readableFee())
+
+            # Now we know what the fee is, we can do it again and finalise it
+            result = delegation_tx.delegate(delegated_uluna)
+            
+            exit()
+            if result == True:
+                delegation_tx.broadcast()
+            
+                if delegation_tx.broadcast_result.is_tx_error():
+                    print (' 🛎️ The delegation failed, an error occurred:')
+                    print (f' 🛎️  {delegation_tx.broadcast_result.raw_log}')
+                else:
+                    print (f' ✅ Delegated amount: {wallet.formatUluna(delegated_uluna, True)}')
+                    print (f' ✅ Tx Hash: {delegation_tx.broadcast_result.txhash}')
+            else:
+                print (' 🛎️  The deleggation could not be completed')
+        else:
+            print ('🛎️  The delegation could not be completed')
+    #print (user_validator)
+    #for validator in validators:
+    #    if validators[validator]['is_jailed'] == False:
+    #        print(validator, validators[validator]['voting_power'])
+
+
+    exit()
     
     # Get the password that decrypts the user wallets
     decrypt_password:str = getpass() # the secret password that encrypts the seed phrase
