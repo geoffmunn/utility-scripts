62b4b02e5ce55b323aad2c376b59b7048e163910
Geoff Munn <geoff@wxoutside.tools>
2023-03-21 21:00:36 +1300

    gas adjustment fee increase and try again

--- send_transaction.py
+++ send_transaction.py
@@ -301,6 +301,14 @@ def main():
                 if result == True:
                     send_tx.broadcast()
                 
+                    if send_tx.broadcast_result.code == 11:
+                        print (' 🛎️  Increasing the gas adjustment fee and trying again')
+                        send_tx.terra.gas_adjustment += 0.5
+                        send_tx.simulate(recipient_address, uluna_amount, memo)
+                        print (send_tx.readableFee())
+                        send_tx.send()
+                        send_tx.broadcast()
+
                     if send_tx.broadcast_result.is_tx_error():
                         print (' 🛎️  The send transaction failed, an error occurred:')
                         print (f' 🛎️  {send_tx.broadcast_result.raw_log}')
