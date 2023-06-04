af34edf61fede7224d8637b0cb4d7db76da32c74
Geoff Munn <geoff@wxoutside.tools>
2023-06-04 20:10:31 +1200

    Zero reward balances should be ignored now

--- manage_wallets.py
+++ manage_wallets.py
@@ -298,7 +298,7 @@ def main():
                     uluna_reward:int = delegations[validator]['rewards'][ULUNA]
 
                     # Only withdraw the staking rewards if the rewards exceed the threshold (if any)
-                    if uluna_reward > wallet.delegations['threshold']:
+                    if uluna_reward > wallet.delegations['threshold'] and uluna_reward > 0:
 
                         print (f'Withdrawing {wallet.formatUluna(uluna_reward, False)} rewards')
 
