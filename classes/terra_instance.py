87f60341ccc79b165519e9a216aa879b048a66db
Geoff Munn <geoff@wxoutside.tools>
2023-10-15 15:25:20 +1300

    Proper var detection in lcd client creation

--- classes/terra_instance.py
+++ classes/terra_instance.py
@@ -10,8 +10,10 @@ from terra_classic_sdk.client.lcd import LCDClient
     
 class TerraInstance:
     def __init__(self):
+        self.chain_id:str   = None
         self.gas_adjustment = float(GAS_ADJUSTMENT)
         self.terra          = None
+        self.url:str        = None
         
     def create(self, denom:str = 'uluna') -> LCDClient:
         """
@@ -19,22 +21,26 @@ class TerraInstance:
         """
         
         if denom in CHAIN_DATA:
-            self.chain_id = CHAIN_DATA[denom]['chain_id']
-            self.url      = CHAIN_DATA[denom]['lcd_urls'][0]
+            if 'chain_id' in CHAIN_DATA[denom]:
+                self.chain_id = CHAIN_DATA[denom]['chain_id']
+
+                if self.chain_id == 'osmosis-1':
+                    gas_prices = '1uosmo,1uluna'
+                else:
+                    gas_prices = None
+                    
+            if 'lcd_urls' in CHAIN_DATA[denom]:
+                self.url      = CHAIN_DATA[denom]['lcd_urls'][0]
             
-            if self.chain_id == 'osmosis-1':
-                gas_prices = '1uosmo,1uluna'
-            else:
-                gas_prices = None
+            if self.chain_id is not None and self.url is not None:
+                terra:LCDClient = LCDClient(
+                    chain_id       = self.chain_id,
+                    gas_adjustment = float(self.gas_adjustment),
+                    url            = self.url,
+                    gas_prices     = gas_prices
+                )
 
-            terra:LCDClient = LCDClient(
-                chain_id       = self.chain_id,
-                gas_adjustment = float(self.gas_adjustment),
-                url            = self.url,
-                gas_prices     = gas_prices
-            )
-
-            self.terra = terra
+                self.terra = terra
         
         return self.terra
 
