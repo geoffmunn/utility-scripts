36b3836f7f587fdef10ddc7bc24d6a13d44242c8
Geoff Munn <geoff@wxoutside.tools>
2023-11-01 14:25:29 +1300

    Chain ID replaced with chain data value

--- classes/terra_instance.py
+++ classes/terra_instance.py
@@ -3,7 +3,8 @@
 
 from constants.constants import (
     CHAIN_DATA,
-    GAS_ADJUSTMENT
+    GAS_ADJUSTMENT,
+    UOSMO
 )
 
 from terra_classic_sdk.client.lcd import LCDClient
@@ -24,13 +25,13 @@ class TerraInstance:
             if 'chain_id' in CHAIN_DATA[denom]:
                 self.chain_id = CHAIN_DATA[denom]['chain_id']
 
-                if self.chain_id == 'osmosis-1':
+                if self.chain_id == CHAIN_DATA[UOSMO]['chain_id']:
                     gas_prices = '1uosmo,1uluna'
                 else:
                     gas_prices = None
                     
             if 'lcd_urls' in CHAIN_DATA[denom]:
-                self.url      = CHAIN_DATA[denom]['lcd_urls'][0]
+                self.url = CHAIN_DATA[denom]['lcd_urls'][0]
             
             if self.chain_id is not None and self.url is not None:
                 terra:LCDClient = LCDClient(
