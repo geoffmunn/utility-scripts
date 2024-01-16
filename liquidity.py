4c995adff746ea431d59154d6537cbf2a307fa51
Geoff Munn <geoff@wxoutside.tools>
2024-01-16 13:53:57 +1300

    Old code removed

--- liquidity.py
+++ liquidity.py
@@ -15,121 +15,12 @@ from constants.constants import (
     JOIN_POOL,
     ULUNA,
     UOSMO,
-    USER_ACTION_CONTINUE,
     USER_ACTION_QUIT,
 )
 
-from classes.wallet import UserWallet
 from classes.wallets import UserWallets
 from classes.liquidity_transaction import LiquidityTransaction
-
-def get_send_to_address(user_wallets:UserWallet):
-    """
-    Show a simple list address from what is found in the user_config file
-    """
-
-    label_widths:list = []
-
-    label_widths.append(len('Number'))
-    label_widths.append(len('Wallet name'))
-
-    for wallet_name in user_wallets:
-        if len(wallet_name) > label_widths[1]:
-            label_widths[1] = len(wallet_name)
-                
-    # Generic string we use for padding purposes
-    padding_str:str   = ' ' * 100
-    header_string:str = ' Number |'
-
-    if label_widths[1] > len('Wallet name'):
-        header_string +=  ' Wallet name' + padding_str[0:label_widths[1] - len('Wallet name')] + ' | Address                                      '
-    else:
-        header_string +=  ' Wallet name | Address                                      '
-
-    horizontal_spacer:str = '-' * len(header_string)
-
-    # Create default variables and values
-    wallets_to_use:dict   = {}
-    user_wallet:dict      = {}
-    recipient_address:str = ''
-
-    while True:
-
-        count:int            = 0
-        wallet_numbers:dict  = {}
-        wallets_by_name:dict = {}
-
-        print ('\n' + horizontal_spacer)
-        print (header_string)
-        print (horizontal_spacer)
-
-        for wallet_name in user_wallets:
-            wallet:UserWallet = user_wallets[wallet_name]
-
-            count += 1
-            wallet_numbers[count] = wallet
-            wallets_by_name[wallet.name.lower()] = count
-
-            if wallet_name in wallets_to_use:
-                glyph = '✅'
-            else:
-                glyph = '  '
-
-            count_str =  f' {count}' + padding_str[0:6 - (len(str(count)) + 2)]
-            
-            wallet_name_str    = wallet_name + padding_str[0:label_widths[1] - len(wallet_name)]
-            wallet_address:str = wallet.address
-            
-            print (f"{count_str}{glyph} | {wallet_name_str} | {wallet_address}")
-            
-        print (horizontal_spacer + '\n')
-
-        print ('You can send to an address in your config file by typing the wallet name or number.')
-        print ('You can also send to a completely new address by entering the wallet address.\n')
-
-        answer:str = input("What is the address you are sending to? (or type 'X' to continue, or 'Q' to quit) ").lower()
-        
-        # Check if someone typed the name of a wallet
-        if answer in wallets_by_name.keys():
-            answer = str(wallets_by_name[answer])
-        
-        if answer.isdigit() and int(answer) in wallet_numbers:
-
-            wallets_to_use:dict = {}
-
-            key:str = wallet_numbers[int(answer)].name
-            if key not in wallets_to_use:
-                wallets_to_use[key] = wallet_numbers[int(answer)]
-            else:
-                wallets_to_use.pop(key)
-        else:
-            # check if this is an address we support:
-            prefix:str = wallet.getPrefix(answer)
-
-            if prefix in wallet.getSupportedPrefixes():
-                recipient_address:str = answer
-                break
-            
-        if answer == USER_ACTION_CONTINUE:
-            if len(wallets_to_use) > 0:
-                break
-            else:
-                print ('\nPlease select a wallet first.\n')
-
-        if answer == USER_ACTION_QUIT:
-            break
-
-    # Get the first (and only) wallet from the list
-    if len(wallets_to_use) > 0:
-        for item in wallets_to_use:
-            user_wallet:UserWallet = wallets_to_use[item]
-            recipient_address:str  = user_wallet.address
-            break
-    
-    return recipient_address, answer
-
 from terra_classic_sdk.core.coin import Coin
-from terra_classic_sdk.core.coins import Coins
 
 def main():
 
