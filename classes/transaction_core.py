06e521ed588ff0f57728030c77fb08d3de30f407
Geoff Munn <geoff@wxoutside.tools>
2024-02-24 17:43:42 +1300

    Lenny send responses correctly identified

--- classes/transaction_core.py
+++ classes/transaction_core.py
@@ -434,8 +434,15 @@ class TransactionCore():
 
                             # GRDX/ULENNY -> ULUNA swaps (will override the standard swaps detection done earlier)
                             if '_contract_address' in log.events_by_type['wasm'] and (GRDX_SMART_CONTRACT_ADDRESS in log.events_by_type['wasm']['_contract_address'] or LENNY_SMART_CONTRACT_ADDRESS in log.events_by_type['wasm']['_contract_address']):
-                                transaction_result.result_sent     = Coin.from_data({'amount': log.events_by_type['wasm']['offer_amount'][0], 'denom': log.events_by_type['wasm']['offer_asset'][0]})
-                                transaction_result.result_received = Coins.from_proto([Coin.from_data({'amount': log.events_by_type['wasm']['return_amount'][0], 'denom': log.events_by_type['wasm']['ask_asset'][0]})])
+                                if 'action' in log.events_by_type['wasm'] and log.events_by_type['wasm']['action'][0] == 'transfer':
+                                    # Sending GRDX/ULENNY to another wallet
+                                    transaction_result.result_sent     = Coin.from_data({'amount': log.events_by_type['wasm']['amount'][0], 'denom': self.denom})
+                                    transaction_result.result_received = Coins.from_proto([Coin.from_data({'amount': log.events_by_type['wasm']['amount'][0], 'denom': self.denom})])
+                                else:
+                                    # Assumes swaps between GRDX/ULENN -> ULUNA
+                                    transaction_result.result_sent     = Coin.from_data({'amount': log.events_by_type['wasm']['offer_amount'][0], 'denom': log.events_by_type['wasm']['offer_asset'][0]})
+                                    transaction_result.result_received = Coins.from_proto([Coin.from_data({'amount': log.events_by_type['wasm']['return_amount'][0], 'denom': log.events_by_type['wasm']['ask_asset'][0]})])
+                                    
                                 transaction_result.log_found       = True
 
                         if transaction_result.log_found == False:
